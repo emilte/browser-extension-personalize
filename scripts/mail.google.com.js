d85becdb6cf0e7c2bfb4dfb9f239c65d78a50758
Emil Telstad <22004178+emilte@users.noreply.github.com>
2025-01-16 10:16:04 +0100

    sync

---
+++ scripts/mail.google.com.js
@@ -0,0 +1,15 @@
+// import { getEventListeners } from 'event-listeners';
+
+// function main() {
+// 	document.addEventListener('keydown', (event) => {
+// 		const archiveBtn = document.querySelector('div[aria-label="Archive"][role="button"]::after');
+// 		// console.log(event.altKey, event.key);
+// 		console.log(getEventListeners(archiveBtn));
+// 		if (event.altKey && event.key === '') {
+// 			console.log('A', archiveBtn);
+// 			archiveBtn.click();
+// 		}
+// 	});
+// }
+// main();
+// document.querySelector('div[aria-label="Archive"][role="button"]::after');
